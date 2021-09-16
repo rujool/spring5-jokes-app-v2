b45503af82596fc471d3687d539b32687e9d6e98
rujdoshi <rujdoshi@cisco.com>
2021-09-16 16:32:30 -0700

    Fixing wiring in service, #2.

--- src/main/java/guru/springframework/jokesapp/services/JokesServiceImpl.java
+++ src/main/java/guru/springframework/jokesapp/services/JokesServiceImpl.java
@@ -1,14 +1,12 @@
 package guru.springframework.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JokesServiceImpl implements JokesService {
 
-    private final ChuckNorrisQuotes chuckNorrisQuotes;
-
-    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
-    }
+    ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
 
     @Override
     public String getRandomQuote() {
